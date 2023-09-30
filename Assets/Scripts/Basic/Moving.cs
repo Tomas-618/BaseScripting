f6c1efc63d239f6e6bb815185f1b217f2a320584
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2023-09-30 22:01:39 +0400

    Удаление метода DrawRay

--- Assets/Scripts/Basic/Moving.cs
+++ Assets/Scripts/Basic/Moving.cs
@@ -7,20 +7,9 @@ public class Moving : MonoBehaviour, IMovable, ISpeedable
 
     public float Speed => _speed;
 
-    private void Update()
-    {
-        DrawRays();
+    private void Update() =>
         Move();
-    }
 
     public void Move() =>
         _transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-
-    private void DrawRays()
-    {
-        float raysDistance = 1;
-
-        Debug.DrawRay(_transform.position, _transform.forward * (raysDistance + _transform.localScale.z), Color.blue);
-        Debug.DrawRay(_transform.position, _transform.up * (raysDistance + _transform.localScale.y), Color.green);
-    }
 }
